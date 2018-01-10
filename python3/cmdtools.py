43218f4099dca3a5054ad56a536a770a090164ee
John Tongelidis <tongeljl@Johns-MacBook-Pro.local>
2018-01-09 20:46:31 -0600

    Added templist

--- python3/cmdtools.py
+++ python3/cmdtools.py
@@ -1,5 +1,5 @@
 
-import readline
+import readline, os, subprocess, tempfile
 
 def history(history_id=None):
     if history_id is None:
@@ -8,5 +8,19 @@ def history(history_id=None):
     else:
         print( str(history_id ) + ': ' + readline.get_history_item(history_id) ) 
 
+def templist():
+    (fd, path) = tempfile.mkstemp()
 
+    editor = os.getenv('EDITOR', 'vi')
+    subprocess.call('%s %s' % (editor, path), shell=True)
 
+    f = open(path, 'r')
+    temp_data = list(f)
+    f.close()
+
+    os.unlink(path)
+
+    for i in range(len(temp_data)):
+        temp_data[i] = temp_data[i].strip().replace('\n', '')
+        
+    return temp_data
