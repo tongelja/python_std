334b2c772802c71f927f52d1f86f39ab60369af4
tongelja <tongelja@hotmail.com>
2018-02-05 10:32:48 -0600

    updates

--- python3/cmdtools.py
+++ python3/cmdtools.py
@@ -5,8 +5,13 @@ def history(history_id=None):
     if history_id is None:
         for i in range(readline.get_current_history_length()):
             print( str(i+1) + ': ' + readline.get_history_item(i+1) ) 
+        return None
+
     else:
-        print( str(history_id ) + ': ' + readline.get_history_item(history_id) ) 
+
+#        print( str(history_id ) + ': ' + readline.get_history_item(history_id) ) 
+        return readline.get_history_item(history_id)
+
 
 def templist():
     (fd, path) = tempfile.mkstemp()
